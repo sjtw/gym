e0c07a84a8c472781d83445b7a869a6982aded5f
sjtw <simonjtweed@gmail.com>
2025-04-26 00:40:19 +0100

    fix max profit test

--- solutions/max_profit_test.py
+++ solutions/max_profit_test.py
@@ -1,11 +1,10 @@
 import unittest
 
 from max_profit import max_profit
-from rotate_array import rotate_array
 
 
 class TestBasicFunctionality(unittest.TestCase):
-    def test_rotate_array(self):
-        a = [1, 2, 3, 4, 5, 6, 7]
-        rotate_array(a, 3)
-        self.assertEqual(a, [5,6,7,1,2,3,4])
+    def test_max_profit(self):
+        self.assertEqual(max_profit([2,10,1,1,3]), 10)
+        self.assertEqual(max_profit([5,4,1]), 0)
+        self.assertEqual(max_profit([1,2,3,3]), 2)
